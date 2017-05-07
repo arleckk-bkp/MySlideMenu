a5d442828b0beb5fdb5fdcc9947b5195855d184b
Oscar Reynaldo Flores Jimenez <arleckk@MacBook-de-Oscar.local>
2017-05-06 21:25:09 -0500

    first version, no animation, no menu, just button and show new view

---
+++ MySlideMenu/SlideMenuButton.swift
@@ -0,0 +1,13 @@
+//
+//  SlideMenuButton.swift
+//  MySlideMenu
+//
+//  Created by Oscar Reynaldo Flores Jimenez on 06/05/17.
+//  Copyright © 2017 Oscar Reynaldo Flores Jimenez. All rights reserved.
+//
+
+import UIKit
+
+class SlideMenuButton: UIBarButtonItem {
+
+}
