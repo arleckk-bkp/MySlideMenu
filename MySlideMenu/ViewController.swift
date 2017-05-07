6004a8b50867f04b53cd10ebd3b509371317d261
Oscar Reynaldo Flores Jimenez <arleckk@MacBook-de-Oscar.local>
2017-05-06 19:53:24 -0500

    Initial Commit

---
+++ MySlideMenu/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MySlideMenu
+//
+//  Created by Oscar Reynaldo Flores Jimenez on 06/05/17.
+//  Copyright © 2017 Oscar Reynaldo Flores Jimenez. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
